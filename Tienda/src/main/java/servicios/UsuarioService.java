ca283ba3c3d19d0465ef6e8a439bc8d7b65ae197
Lucy <marygaray16@@gmail.com>
2019-12-18 14:19:48 -0500

    Servicios de usuarios, insertar y listar

---
+++ Tienda/src/main/java/servicios/UsuarioService.java
@@ -0,0 +1,47 @@
+package servicios;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+
+import dao.UsuarioDAO;
+import modelo.Categoria;
+import modelo.Usuario;
+
+@Path("/usuarios")
+public class UsuarioService {
+	@Inject
+	private UsuarioDAO usuarioDAO;
+	
+	@GET
+	@Path("getUsuarios")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Usuario> getUsuario() {
+		return usuarioDAO.listadoUsuarios();
+	}
+	
+	@POST
+	@Path("setUsuarios")
+	@Produces("application/json")
+	@Consumes("application/json")
+	public Respuesta createUsuario(Usuario usuario) {
+		Respuesta r = new Respuesta();
+		try {
+			usuarioDAO.insertar(usuario);
+			r.setCodigo(0);
+			r.setMensajes("OK");
+		} catch (Exception e) {
+			r.setCodigo(99);
+			r.setMensajes("Error Insertar");
+		}
+		return r;
+	}
+
+}
